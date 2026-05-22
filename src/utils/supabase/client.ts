8a84d547d55f58b7bf388c91b16c2e58216f2480
denypm <denypm@users.noreply.github.com>
2026-05-22 09:10:59 +0700

    fix: hardcode supabase keys to bypass vercel env config

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -2,7 +2,7 @@ import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
   return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    "https://hwuxlxqmaqowtpvcjmdc.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh3dXhseHFtYXFvd3RwdmNqbWRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1MDE4NTQsImV4cCI6MjA5NDA3Nzg1NH0.U1vptiMEZh52VsPAb6S1nNvPfroZkTHEfo_zL1cl_U0"
   )
 }
--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -5,8 +5,8 @@ export async function createClient() {
   const cookieStore = await cookies()
 
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    "https://hwuxlxqmaqowtpvcjmdc.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh3dXhseHFtYXFvd3RwdmNqbWRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1MDE4NTQsImV4cCI6MjA5NDA3Nzg1NH0.U1vptiMEZh52VsPAb6S1nNvPfroZkTHEfo_zL1cl_U0",
     {
       cookies: {
         getAll() {
